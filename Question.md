6c76a20690ed14d92f9343f1b5b4f263ae4f6e90
Galland_Davd <94803406+GallandDavid@users.noreply.github.com>
2022-11-26 19:45:09 +0100

    Upload files

---
+++ Question.md
@@ -0,0 +1,15 @@
+1]
+
+- le facteur de branchement est de 3
+- non, toutes les branches d'un arbre ne doivent pas obligatoirement être de la même hauteur
+
+2]
+  
+- le meilleur plateau pour Ami serait celui qui comporte 8
+- le meilleur plateau pour Ennemi serait celui qui comporte -4
+- comme les deux noeuds qui ont qu'un seul fil, sont un coup pour Ami, ce n'est pas une bonne chose pour ami car il n'aura que le choix remonté par ennemi; donc il ne pourras pas maximiser à ces noeuds
+
+3]
+
+- ? devrait prendre la valeur 3 pour pouvoir élaguer la branche antèrieur
+-
---
+++ starter-chess.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import time
+import chess
+from random import choice
+
+
+#------ Random methode -------#
+
+def randomMove(b):
+    '''Renvoie un mouvement au hasard sur la liste des mouvements possibles. Pour avoir un choix au hasard, il faut
+    construire explicitement tous les mouvements. Or, generate_legal_moves() nous donne un itérateur.'''
+    return choice([m for m in b.generate_legal_moves()])
+
+def deroulementRandom(b):
+    '''Déroulement d'une partie d'échecs au hasard des coups possibles. Cela va donner presque exclusivement
+    des parties très longues et sans gagnant. Cela illustre cependant comment on peut jouer avec la librairie
+    très simplement.'''
+    print("----------")
+    print(b)
+    if b.is_game_over():
+        print("Resultat : ", b.result())
+        return
+    b.push(randomMove(b))
+    deroulementRandom(b)
+    b.pop()
+
+#------ Fin Random methode -------#
+
+#------ Depth methode -------#
+
+nb_noeuds = 0
+    
+def deroulementExhaustif(b,p, nb_noeud):
+    
+    if b.is_game_over() or p == 0:
+        return nb_noeud
+    for m in b.generate_legal_moves():
+        b.push(m)
+        nb_noeud = deroulementExhaustif(b,p - 1, nb_noeud + 1)
+        b.pop()
+    return nb_noeud
+
+def maxProfondeur(b):
+    p_max = 0
+    for p in range (0, 12):
+        time_start = time.time()
+        nb_noeuds = deroulementExhaustif(b,p,0)
+        print_str = "for profondeur " + str(p) + " have " + str(nb_noeuds) + " noeuds"
+        print(print_str)
+        time_after = time.time()
+        if(time_after - time_start > 30):
+            p_max = p -1
+            break
+    return p_max
+
+
+#------ Fin Depth methode -------#
+
+#------ MinMax methode -------#
+
+
+def eval(board):
+    valeurs = {'K':200, 
+               'Q':9, 
+               'R':5, 
+               'B':3, 
+               'N':3, 
+               'P':1,
+               '.':0}
+    
+    score = 0
+    for k,p in board.piece_map().items():
+        if(p.symbol() == p.symbol().upper()):
+            score += valeurs[p.symbol()]
+            score +=  (k//8) * 0.2
+        else:
+            score -= valeurs[p.symbol().upper()]
+            score -= 1 - ((k//8) * 0.2)
+    return score
+
+def MaxMin(b,p):
+    if b.is_game_over():
+        if(b.result() == "1-0"):
+            return 1000
+        elif(b.result() == "0-1"):
+            return -1000
+        return 0
+    if p == 0:
+        return eval(b)
+    max = -10000
+    for m in b.generate_legal_moves():
+        b.push(m)
+        v = MinMax(b,p-1)
+        if(v > max):
+            max = v
+        b.pop()
+    return max
+        
+def MinMax(b,p):
+    if b.is_game_over():
+        if(b.result() == "1-0"):
+            return 1000
+        elif(b.result() == "0-1"):
+            return -1000
+        return 0
+    if p == 0:
+        return eval(b)
+    min = 10000
+    for m in b.generate_legal_moves():
+        b.push(m)
+        v = MaxMin(b,p-1)
+        if(v < min):
+            min = v
+        b.pop()
+    return min
+
+def init(b,p,player):
+    coup = 0
+    if(player):
+        value = -100000
+        for m in b.generate_legal_moves():
+            b.push(m)
+            v = MaxMin(b,p-1)
+            if(v > value):
+                value = v
+                coup = m
+            b.pop()
+    else:
+        value = 100000
+        for m in b.generate_legal_moves():
+            b.push(m)
+            v = MinMax(b,p-1)
+            if(v < value):
+                value = v
+                coup = m
+            b.pop()
+    return coup   
+
+def playGame(b,p,player):
+    print("----------")
+    print(b)
+    if(player):
+        b.push(init(b,p,player))
+    else:
+        b.push(init(b,p,player))
+
+    if b.is_game_over():
+        return b
+    if(player):
+        player = False
+    else:
+        player = True
+    playGame(b,p,player)
+    
+
+
+#------ Fin MinMax methode -------#
+
+
+board = chess.Board()
+
+#deroulementRandom(board)
+#print( maxProfondeur(board))
+#deroulementExhaustif(board,2)
+
+playGame(board,3,True)
+print("Resultat : " + board.result())
+
